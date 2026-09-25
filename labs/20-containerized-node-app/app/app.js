e603aeb3131fbd9a05a443fe7a072df18c4e4552
renauale <alexandre.renaud@etu.univ-smb.fr>
2026-09-25 11:37:33 +0200

    TP2

--- labs/20-containerized-node-app/app/app.js
+++ labs/20-containerized-node-app/app/app.js
@@ -21,14 +21,19 @@ export async function buildApp() {
   // Health check endpoint
   app.get("/health", async () => ({ ok: true }));
 
+  app.get("/api/quotes", async () => {
+    const result = await app.pg.query("SELECT * FROM quotes");
+    return result.rows;
+  });
+
   // Get all quotes endpoint
   app.get("/", async (_req, reply) => {
     /******* TODO SELECT quotes from DB ******/
-    // const result = await app.pg.query('SELECT * FROM quotes')
-    // const quotes = result.rows;
+    const result = await app.pg.query('SELECT * FROM quotes')
+    const quotes = result.rows;
 
     // Placeholder test quotes
-    const quotes = [{text: "quote 1", author: "author 1"}, {text: "quote 2", author: "author 2"}]; 
+    // const quotes = [{text: "quote 1", author: "author 1"}, {text: "quote 2", author: "author 2"}]; 
     return reply.view("index.hbs", { quotes });
   });
 
@@ -43,12 +48,14 @@ export async function buildApp() {
     }
 
     /******* TODO INSERT quote into DB ******/
-    // await app.pg.query('INSERT INTO quotes (author, text) VALUES ($1, $2)', [author || "anonymous", text]);
+    await app.pg.query('INSERT INTO quotes (author, text) VALUES ($1, $2)', [author || "anonymous", text]);
 
     app.log.info({quote: { author: author || "anonymous", text }}, 'New quote added');
 
     return reply.redirect("/");
   });
+
+  
   
   return app;
 };
